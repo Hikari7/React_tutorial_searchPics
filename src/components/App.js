c14d3c17472d324112aa3a60af9a7adb6f5a9956
Hikari7 <h.kobe712@gmail.com>
2022-08-15 23:25:32 -0700

    fetch unsplash data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmint(term) {
+    console.log(term);
+    //API取るよ！
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID CG-6TrHE3gcTpcpECn48aFwfcKv26qj7Epgm0fsqRUY",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmint} />
+      </div>
+    );
+  }
+}
 
 export default App;
